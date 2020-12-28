e02ed890e564002205710f13087087b22c7cb449
hmzhan <hmzhan@gmail.com>
2020-12-28 14:20:31 -0500

    kaggle data and model

--- kaggle_tweet/data.py
+++ kaggle_tweet/data.py
@@ -1,4 +1,17 @@
-import numpy as np
 import pandas as pd
-import tensorflow as tf
-import tensorflow_hub as hub
+
+
+class TweetData:
+    def __init__(self):
+        self.train = None
+        self.test = None
+        self.submission = None
+
+    def load_data(self):
+        """
+        Load tweeter data
+        :return: None
+        """
+        self.train = pd.read_csv('/kaggle_data/train.csv')
+        self.test = pd.read_csv('/kaggle_data/test.csv')
+        self.submission = pd.read_csv('/kaggle_data/sample_submission.csv')
---
+++ kaggle_tweet/model.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+
+
+class BertTweeterModel:
+    def __init__(self):
+        pass
